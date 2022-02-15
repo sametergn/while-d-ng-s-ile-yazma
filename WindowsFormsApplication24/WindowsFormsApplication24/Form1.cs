1f6156fba5e7fdd4806d427359d5c308b6920c1b
sametergn <99245370+sametergn@users.noreply.github.com>
2022-02-15 11:55:58 +0300

    Add files via upload

---
+++ WindowsFormsApplication24/WindowsFormsApplication24/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int i = 0;
+            int s1;
+           
+            while(i < Convert.ToInt32(textBox2.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                s1 = Convert.ToInt32(textBox2.Text);
+                i++;
+            }
+        }
+    }
+}
